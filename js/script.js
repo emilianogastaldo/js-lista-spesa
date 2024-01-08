bf79b1898ce968c6a050310c74f6ce0fd1ce001c
emilianogastaldo <emiliano0303@gmail.com>
2024-01-08 14:49:59 +0100

    added list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 console.log('js ok');
 
 const shoppingList = ['Pane', 'Uova', 'Latte', "Olio extravergine d'oliva", 'Sale', 'Zucchero', 'Farina 0', 'Farina 00', 'Lievito secco', 'Lievito per dolci', 'Caffè', 'Cacao amaro'];
+const container = document.querySelector('.container');
 
+
+let mailList = '<ul>';
+let elementList = '';
+let i = 0;
+// for (let i = 0; i < shoppingList.length; i++) {
+//     elementList += `<li>${shoppingList[i]}</li>`;
+// }
+
+while (i < shoppingList.length) {
+    elementList += `<li>${shoppingList[i]}</li>`
+    i++
+}
+mailList += `${elementList}</ul>`;
+
+container.innerHTML = mailList;
