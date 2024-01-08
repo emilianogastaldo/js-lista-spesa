bc7fd33cb5893077b45e10eb8933771214d89fc3
emilianogastaldo <emiliano0303@gmail.com>
2024-01-08 14:38:39 +0100

    creo la lista della spesa

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
 console.log('js ok');
+
+const shoppingList = ['Pane', 'Uova', 'Latte', "Olio extravergine d'oliva", 'Sale', 'Zucchero', 'Farina 0', 'Farina 00', 'Lievito secco', 'Lievito per dolci', 'Caffè', 'Cacao amaro'];
+
